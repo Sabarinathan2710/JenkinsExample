9448248aeacbbf60f646999308ca8f18a1ee4938
Sabarinathan2710 <sabarinathanv.professional@gmail.com>
2025-04-02 10:59:01 +0530

    First commit

---
+++ src/main/java/com/jenkinsproject/org/SpringJenkinsSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsproject.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsSampleProjectApplication.class, args);
+	}
+
+}
